bd048d770221553d191dee70241ddc84db485b67
정재희 <jaeheechung@Jaybook.local>
2025-10-01 22:23:59 +0900

    recreate venv

--- backend/myvenv/bin/activate.fish
+++ backend/myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jaeheechung/my_project/myvenv"
+set -gx VIRTUAL_ENV "/Users/jaeheechung/my_project/backend/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
